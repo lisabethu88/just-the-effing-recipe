a1d8d2e53527268fe3742aa10f9782f559811aab
Lisa Utsett <lisabethutsett@gmail.com>
2024-11-18 19:39:51 -0500

    Add data folder and file

---
+++ src/data/Data.js
@@ -0,0 +1,29 @@
+import main_course from "../assets/main-course.jpg";
+import breakfast from "../assets/breakfast.jpg";
+import lunch from "../assets/lunch.jpg";
+import snacks from "../assets/snacks.jpg";
+import desserts from "../assets/desserts.jpg";
+import appetizer from "../assets/appetizer.jpg";
+import salad from "../assets/salad.jpg";
+import soup from "../assets/soup.jpg";
+import side_dish from "../assets/side-dish.jpg";
+import beverage from "../assets/beverage.jpg";
+import finger_food from "../assets/finger-food.jpg";
+import bread from "../assets/bread.jpg";
+import drink from "../assets/drink.jpg";
+
+export const categories = [
+  { title: "Breakfast", image: breakfast },
+  { title: "Lunch", image: lunch },
+  { title: "Main Course", image: main_course },
+  { title: "Snacks", image: snacks },
+  { title: "Dessert", image: desserts },
+  { title: "Appetizer", image: appetizer },
+  { title: "Salad", image: salad },
+  { title: "Soup", image: soup },
+  { title: "Side Dish", image: side_dish },
+  { title: "Beverage", image: beverage },
+  { title: "Fingerfood", image: finger_food },
+  { title: "Bread", image: bread },
+  { title: "Drinks", image: drink },
+];
