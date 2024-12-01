e905f990c2de9caeb9110d430b96796a8dcc30dd
Lisa Utsett <lisabethutsett@gmail.com>
2024-12-01 16:12:58 -0500

    Update index.css, index.js, and App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
 import "./App.css";
 import Home from "./pages/Home";
+import NavigationHeader from "./components/NavigationHeader";
+import { Routes, Route } from "react-router-dom";
+import Footer from "./components/Footer";
+import About from "./pages/About";
+import Categories from "./pages/Categories";
+import Subcategories from "./pages/Subcategories";
+import Recipes from "./pages/Recipes";
+import Recipe from "./pages/Recipe";
 
 function App() {
   return (
-    <div>
-      <header></header>
-      <Home></Home>
-    </div>
+    <>
+      <NavigationHeader />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/categories" element={<Categories />} />
+        <Route path="/categories/:category" element={<Subcategories />} />
+        <Route
+          path="/categories/:category/:subcategory"
+          element={<Recipes />}
+        />
+        <Route
+          path="/categories/:category/:subcategory/:recipeId"
+          element={<Recipe />}
+        />
+        <Route path="*" element={<Home />} /> {/* Handles unmatched routes */}
+      </Routes>
+
+      <Footer />
+    </>
   );
 }
 
